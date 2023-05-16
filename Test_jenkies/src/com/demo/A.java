734071ff7d553c59f8cec221993dd8d0442228dc
Lenovo <Lenovo@Amogh>
2023-05-16 13:03:57 +0530

    as

---
+++ Test_jenkies/src/com/demo/A.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+public class A 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hello");
+		
+	}
+	public void m1()
+	{
+	System.out.println("hello cjc");
+	}
+
+}
